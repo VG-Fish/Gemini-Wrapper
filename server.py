c6ff5d3b0d3c207fb2e2dd8aa79b20ee733a945c
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-04-24 09:50:06 -0500

    fleshed out server.py

--- server.py
+++ server.py
@@ -1,2 +1,16 @@
-import flask, environs
+from flask import Flask
+from environs import env
 from google import genai
+
+app: Flask = Flask(__name__)
+app.debug = False
+
+env.read_env()
+gemini_api_key: str = env.str("GEMINI_API_KEY")
+
+client = genai.Client(api_key=gemini_api_key)
+
+# response = client.models.generate_content(
+#     model="gemini-2.0-flash-001", contents=
+# )
+# print(response.text)
